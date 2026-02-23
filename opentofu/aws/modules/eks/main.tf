2b635788d4bf9c5ce2ec78f0d0084f974b79050a
vinodbhorge <vinodbbhorge@gmail.com>
2026-02-23 10:26:19 +0530

    Added and commneted out ws-load-balancer controller in eks.

--- opentofu/aws/modules/eks/main.tf
+++ opentofu/aws/modules/eks/main.tf
@@ -169,6 +169,28 @@ module "ebs_csi_driver_irsa" {
 }
 
 # -------------------------------
+# AWS Load Balancer Controller IRSA role
+# -------------------------------
+
+# module "aws_load_balancer_controller_irsa" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+#   version = "~> 5.48"
+  
+#   role_name_prefix = "${local.cluster_name}-aws-lb-controller-"
+  
+#   attach_load_balancer_controller_policy = true
+  
+#   oidc_providers = {
+#     main = {
+#       provider_arn               = aws_iam_openid_connect_provider.oidc.arn
+#       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+#     }
+#   }
+  
+#   tags = local.common_tags
+# }
+
+# -------------------------------
 # kubeconfig updater (local-exec)
 # -------------------------------
 
@@ -211,6 +233,38 @@ resource "aws_eks_addon" "ebs_csi" {
     aws_eks_node_group.default
   ]
 }
+
+# -------------------------------
+# AWS Load Balancer Controller
+# Installs the controller via Helm chart
+# -------------------------------
+
+# resource "helm_release" "aws_load_balancer_controller" {
+#   name       = "aws-load-balancer-controller"
+#   repository = "https://aws.github.io/eks-charts"
+#   chart      = "aws-load-balancer-controller"
+#   namespace  = "kube-system"
+#   version    = "1.10.2"
+
+#   values = [
+#     yamlencode({
+#       clusterName = aws_eks_cluster.cluster.name
+#       serviceAccount = {
+#         create = true
+#         name   = "aws-load-balancer-controller"
+#         annotations = {
+#           "eks.amazonaws.com/role-arn" = module.aws_load_balancer_controller_irsa.iam_role_arn
+#         }
+#       }
+#     })
+#   ]
+
+#   depends_on = [
+#     module.aws_load_balancer_controller_irsa,
+#     aws_eks_cluster.cluster,
+#     aws_eks_node_group.default
+#   ]
+# }
 
 # Create internal load balancer for private ingress
 # resource "kubernetes_service" "private_lb_placeholder" {
